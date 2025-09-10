ed330a72a3b659f929ba95986cf7c02c9eed1bfc
marialuara <maria.luara.santos@escola.pr.gov.br>
2025-09-10 13:36:01 +0000

    criamos o js da aula 7

---
+++ peguntas.js
@@ -0,0 +1,52 @@
+criaCartao(
+    'Programação',
+    'o que é css?',
+    ' é uma linguagem de programação ultilizada para dar estilo a página'
+)
+
+criaCartao(
+    'Programação',
+     'o que é javaScript?',
+    'o javaScript é uma linguagem de programação que nos permite interagir com a página'
+)
+
+criaCartao(
+    'Programação',
+    'o que é html?',
+    'é uma linguagem de programação que nos permite criar a estrutura do nosso site'
+)
+
+criaCartao(
+    'Programação',
+    'para que serve a tag article?',
+    'para criar um conteúdo ou elemento idependente. no nosso caso estamos usando para criar os cartões de estudo'
+)
+
+criaCartao(
+    'Programação',
+    'o qual a diferença entre a tag tittle que usamos na tag head e a tag h1 que usamos tag body?',
+    'a tittle aparecera na aba da página que será aberta e a tag de titulo h1 é usada no corpo do site como titulo principal da página'
+) 
+criaCartao(
+    'Programação',
+    'qual a diferença entre a tags head e header?',
+    'head é a tag de configuraçoes, tudoque fizemos dentro dela, não aparecerá na página do noso site. header é o cabeçalho da página'
+)
+
+criaCartao(
+    'Programação',
+    'o que é a tag footer?',
+    'é a tag utilizada para criar o rodapé'
+)
+
+criaCartao(
+    'Programação',
+    'para que serve o :root ultilizado no css?',
+    'serve para salvar variáveis para serem ultilizadas durante o desenvolvimento da página, como por exemplo, cores e fontes '
+)
+
+criaCartao(
+    'Programação',
+    'qual a diferença entre margin e padding?',
+    'mangin é espaçamento externo enquanto o padding é espaçamento interno'
+)
